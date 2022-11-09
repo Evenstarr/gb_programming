d4667b37bdc1346a56a193800b93cf5540f40b25
Evenstar <alhe@yandex.ru>
2022-11-09 23:05:28 +0300

    Lesson4, tasks 2,3

---
+++ Task4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int IntInput(string message) {
+    Console.Write(message);
+
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = IntInput("Введите число: ");
+
+int sum = 0;
+
+while (n > 0) {
+    sum += n % 10; 
+    n /= 10;
+}
+
+Console.WriteLine($"Сумма цифр введенного числа = {sum}");
---
+++ Task4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int IntInput(string message) {
+    Console.Write(message);
+
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] arr) {
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++) {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+
+int[] FillArray(int len) {
+    int[] arr = new int[len]; 
+    for (int i = 0; i < len; i++) {
+        arr[i] = new Random().Next(0, 1000);
+    }
+    return arr;
+}
+
+int n = IntInput("Введите длину массива: ");
+
+
+PrintArray(FillArray(n));
