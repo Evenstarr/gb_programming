8c87fbc5b085341bdf07078031efbe745a2fef15
Evenstar <alhe@yandex.ru>
2022-11-27 21:54:27 +0300

    Lesson9 initial

---
+++ Task8_7extra/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Доп. задача 61*: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+*/
+
+int IntInput(string message) {
+    Console.Write(message);
+
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = IntInput("Введите размер: ");
+MakeTriange(size);
+
+void MakeTriange(int size) {
+    int val = 1;
+    for(int i = 0; i < size; i++) {
+        for(int blank = 1; blank <= size - i; blank++)
+            Console.Write(" ");
+            for(int j = 0; j <= i; j++) {
+                if (j == 0||i == 0)
+                    val = 1;
+                else
+                    val = val * (i-j+1) / j;
+                Console.Write(val + " ");
+            }
+    Console.WriteLine();
+    }
+}
---
+++ Task9_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int IntInput(string message) {
+    Console.Write(message);
+
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = IntInput("Введите m: ");
+int n = IntInput("Введите n: ");
+
+Console.WriteLine(Sum(m, n));
+
+int Sum(int m, int n) {
+    if (m == n) {
+        return m;
+    } else {
+        return n + Sum(m, n - 1);
+    }    
+}
